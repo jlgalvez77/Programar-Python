5d65e4265d733bf61d8c74c3d677769a51ff44c3
Jose Galvez <jlgalvez77@gmail.com>
2025-07-02 17:08:52 +0200

    02/07/25-Portatil

---
+++ 5. Condicionales/elif.py
@@ -0,0 +1,14 @@
+letra = ''
+
+if letra.lower() == 'a':
+    print('La vocal es la A')
+elif letra.lower() == 'e':
+    print('La vocal es la E')
+elif letra.lower() == 'i':
+    print('La vocal es la I')
+elif letra.lower() == 'o':
+    print('La vocal es la O')
+elif letra.lower() == 'u':
+    print('La vocal es la U')
+else:
+    print('No es ninguna vocal')
---
+++ 5. Condicionales/if_else.py
@@ -0,0 +1,6 @@
+edad = 0
+
+if edad >= 18:
+    print('Eres mayor de edad')
+else:
+    print('No eres mayor de edad')
